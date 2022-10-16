ffa7991f790922797bd601faee3a077fd5d0e425
William Nadela <williamnadela685@gmail.com>
2022-10-17 02:59:36 +0800

    BABYWILLIAM

--- TicketBooth/registration/forms.py
+++ TicketBooth/registration/forms.py
@@ -16,15 +16,7 @@ class CustomerForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(CustomerForm, self).__init__(*args, **kwargs)
         self.instance.type = self.type
-        self.fields['middlename'].required = False,
-
-    def __init__(self, *args, **kwargs):
-        super(CustomerForm, self).__init__(*args, **kwargs)
-        self.instance.type = self.type
-        self.fields['address'].required = True,
-
-
-
+        self.fields['middlename'].required = False
 
     def clean_password(self):
         password = self.data.get('password')
