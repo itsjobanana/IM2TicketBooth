dfc6e4d011fd781f946fddf4d2b49978982902ad
Allenard <89629034+Allenard@users.noreply.github.com>
2022-10-16 23:42:47 +0800

    aaaaaaa

--- TicketBooth/bookticket/urls.py
+++ TicketBooth/bookticket/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.HomeBookTicket.as_view(), name='bookTicket_Index'),  # 127.0.0.1/bookticket
     path('bookMovie', views.MovieBookingTicket.as_view(), name='movie_Ticket'),  # 127.0.0.1/bookMovie
     path('bookConcert', views.ConcertBookingTicket.as_view(), name='concert_Ticket'),  # 127.0.0.1/bookConcert
+    path('displayBooking', views.DisplayBooking.as_view(), name='display_Booking'),
     ]
--- TicketBooth/bookticket/views.py
+++ TicketBooth/bookticket/views.py
@@ -1,8 +1,10 @@
+from django.db import connection
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views import View
 
 from bookticket.forms import ConcertBookingForm, MovieBookingForm
+from registration.models import Concert, Customer, Movie
 
 
 # Create your views here.
@@ -18,27 +20,66 @@ class MovieBookingTicket(View):
     template = 'bookTicket_Movie.html'
 
     def get(self, request):
+        allMovie = Movie.objects.all()
         form = MovieBookingForm()
-        return render(request, self.template, {'form': form})
+        return render(request, self.template, {'movie': allMovie, 'form': form})
 
     def post(self, request):
+        errorMsg = ''
         form = MovieBookingForm(request.POST)
-        if form.is_valid():
+        allMovie = Movie.objects.all()
+        bookingID = request.POST['movieID']
+        print('MovieID: ' + bookingID)
+        numberOfAttendees = Movie.objects.filter(bookingm__movieID=bookingID).count()
+        movie = Movie.objects.get(pk=bookingID)
+        print('Number of Attendees: ' + str(numberOfAttendees))
+        if numberOfAttendees < movie.capacity:
+            customer = Customer.objects.get(pk=request.session['username'])
+            attendMovie = form.save(commit=False)
+            attendMovie.movieUser = customer
             form.save()
-            return redirect(reverse('bookMovie:index'))
-        return render(request, self.template, {'form': form})
+        else:
+            errorMsg = 'Movie is already full!'
+
+        return render(request, self.template, {'movies': allMovie, 'form': form, 'errorMsg': errorMsg})
 
 
 class ConcertBookingTicket(View):
     template = 'bookTicket_Concert.html'
 
     def get(self, request):
+        allConcert = Concert.objects.all()
         form = ConcertBookingForm()
-        return render(request, self.template, {'form': form})
+        return render(request, self.template, {'form': form, 'concert': allConcert})
 
     def post(self, request):
+        errorMsg = ''
         form = ConcertBookingForm(request.POST)
-        if form.is_valid():
+        allConcert = Concert.objects.all()
+        bookingID = request.POST['concertID']
+        print('ConcertID: ' + bookingID)
+        numberOfAttendees = Concert.objects.filter(bookingc__concertID=bookingID).count()
+        concert = Concert.objects.get(pk=bookingID)
+        print('Number of Attendees: ' + str(numberOfAttendees))
+        if numberOfAttendees < concert.capacity:
+            customer = Customer.objects.get(pk=request.session['username'])
+            attendConcert = form.save(commit=False)
+            attendConcert.concertUser = customer
             form.save()
-            return redirect(reverse('bookConcert:index'))
-        return render(request, self.template, {'form': form})
+        else:
+            errorMsg = 'Concert is already full!'
+
+        return render(request, self.template, {'form': form, 'errorMsg': errorMsg, 'concerts': allConcert})
+
+
+class DisplayBooking(View):
+    template = 'displayBooking.html'
+
+    def get(self, request):
+        cursorConcert = connection.cursor()
+        cursorConcert.callproc('ticketbooth.displayConcertEvent', [request.session['username']])
+        displayConcert =cursorConcert.fetchall()
+        cursorConcert.close()
+
+        form = ConcertBookingForm()
+        return render(request, self.template, {'concert': displayConcert, 'form':form})
